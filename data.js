1f064d5a7b015cd778a29f630038671eab2348a2
Adrian <adrianmedvetchi@gmail.com>
2024-06-09 16:57:44 +0300

    first init

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = `
+[
+    {
+        "header": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "image": "./img/img1.png"
+    },
+    {
+        "header": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "image": "./img/img2.png"
+    },
+    {
+        "header": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "image": "./img/img7.png"
+    },
+    {
+        "header": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "image": "./img/img4.png"
+    },
+    {
+        "header": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "image": "./img/img5.png"
+    },
+    {
+        "header": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price": 52.00,
+        "image": "./img/img6.png"
+    }
+]
+`
